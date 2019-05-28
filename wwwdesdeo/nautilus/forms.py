487863a7be54eebc4a32cf00b02f74eb13fd9c79
Giovanni Misitano <gialmisi@student.jyu.fi>
2019-05-29 00:20:42 +0300

    Continued to work on the river pollution example. Can now show the results of the first iteration.

--- wwwdesdeo/nautilus/forms.py
+++ wwwdesdeo/nautilus/forms.py
@@ -10,3 +10,15 @@ class InitializationForm(forms.Form):
         label="Numbers of points to be generated",
         max_value=100,
         initial=5)
+
+
+class InteractiveForm(forms.Form):
+    preferred_point = forms.ChoiceField()
+    
+    def __init__(self, points, *args, **kwargs):
+        super(forms.Form, self).__init__(*args, **kwargs)
+        print("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkj")
+        print(points)
+        self.fields['preferred_point'].choices = [(0, value[0]) for value in points]
+
+
